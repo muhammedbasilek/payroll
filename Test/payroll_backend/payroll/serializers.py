adbec33cd0158be03cee668f94d0e6134361679b
mohaek92 <mohammed.ek@non.keysight.com>
2023-09-12 22:24:04 +0530

    minor changes in last commit

--- Test/payroll_backend/payroll/serializers.py
+++ Test/payroll_backend/payroll/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 from .models import *
 class EmployeeReportSerializer(serializers.ModelSerializer):
+    report_id = serializers.SerializerMethodField()
+    payPeriod = serializers.SerializerMethodField()
+    employeeId = serializers.SerializerMethodField()
+    amountPaid = serializers.SerializerMethodField()
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = ("employeeId","amountPaid","hoursworked","payPeriod","report_id")
+    def get_report_id(self, obj):
+        return obj.report.report_id
+    def get_payPeriod(self, obj):
+        dateObj = {}
+        dateObj["startDate"] = obj.start_date
+        dateObj["endDate"] = obj.end_date
+        return dateObj
+    def get_employeeId(self, obj):
+        return obj.employee_id
+    def get_amountPaid(self, obj):
+        return "$" + str(obj.amount)
--- Test/payroll_backend/payroll/urls.py
+++ Test/payroll_backend/payroll/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-        path('employee-report/', EmployeeReportView.as_view(), name="employee-report"),
+        path('get-report/', EmployeeReportView.as_view(), name="get-report"),
         path('create-employee-report/', EmployeeCreateView.as_view(), name="create-employee-report")
     ]
